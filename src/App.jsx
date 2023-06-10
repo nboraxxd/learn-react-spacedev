5efe991b1a4312f24322041fbd0dd2f9b358b505
bora <nhatbaoxxd@gmail.com>
2023-06-10 21:47:42 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import { useState } from 'react'
+import './assets/css/tailwind.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return <div className='text-5xl text-pink-600'>app</div>
+}
+
+export default App
