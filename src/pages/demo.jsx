a4f808675f742c5b120b09a4e57a36ff702a9342
bora <nhatbaoxxd@gmail.com>
2023-06-18 12:41:39 +0700

    Learning context
    - local state and global state
    - setup context, provider
    - using useContext
    - some notes when using useContext

---
+++ src/pages/demo.jsx
@@ -0,0 +1,67 @@
+import React, { createContext, useContext, useState } from 'react'
+import { styled } from 'styled-components'
+import { Button } from '../components/Button'
+
+const CountContext = createContext({})
+
+export const Demo = () => {
+  const [count, setCount] = useState(0)
+
+  const onIncre = () => {
+    setCount(count + 1)
+  }
+
+  const onDecre = () => {
+    if (count === 0) return
+
+    setCount(count - 1)
+  }
+
+  return (
+    <CountContext.Provider value={{ count: 10, onIncre, onDecre }}>
+      <div className="p-[100px]">
+        <Count />
+
+        <CountContext.Provider value={{ count, onIncre, onDecre }}>
+          <Count />
+        </CountContext.Provider>
+      </div>
+    </CountContext.Provider>
+  )
+}
+
+const CountStyle = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  column-gap: 20px;
+  width: 500px;
+  height: 500px;
+  padding: 40px;
+  border: 1px solid #ccc;
+  font-size: 100px;
+`
+
+const Count = () => {
+  const { count, onIncre, onDecre } = useContext(CountContext)
+
+  const formattedCount = `0${count}`.slice(-2)
+
+  return (
+    <CountStyle>
+      <Button
+        className="flex justify-center items-center pb-2 text-4xl mt-[25px]"
+        onClick={onDecre}
+      >
+        -
+      </Button>
+      {formattedCount}
+      <Button
+        className="flex justify-center items-center pb-2 text-4xl mt-[25px]"
+        onClick={onIncre}
+      >
+        +
+      </Button>
+    </CountStyle>
+  )
+}
