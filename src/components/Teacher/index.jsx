76ce07dcb35948107bef150219a6a812a9cbfad6
bora <nhatbaoxxd@gmail.com>
2023-06-23 15:50:58 +0700

    detail course page

---
+++ src/components/Teacher/index.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const Teacher = ({ avatar, title, position, description, website }) => {
+  return (
+    <div className="teaches">
+      <div className="teacher">
+        <div className="avatar">
+          <img src={avatar} alt={title} />
+        </div>
+        <div className="info">
+          <div className="name">{title}</div>
+          <div className="title">{position}</div>
+          <p className="intro" dangerouslySetInnerHTML={{ __html: description }} />
+          {website && (
+            <p>
+              <strong>Website:</strong>{' '}
+              <a href="https://dangthuyenvuong.github.io/" rel="noopener" target="_blank">
+                https://dangthuyenvuong.github.io/
+              </a>
+            </p>
+          )}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Teacher
--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,16 +1,19 @@
+import { PATH } from '@/config/path'
 import React from 'react'
+import { Link } from 'react-router-dom'
 
-export const Page404 = () => {
+export const Page404 = ({ children }) => {
+  console.log('🥴 ~ Page404 ~ children:', children)
   return (
     <main id="main">
       <div className="notfound">
         <div className="container">
           <section>
             <h2 className="main-title">404</h2>
-            <p>Không tìm thấy trang</p>
-            <a href="/" className="btn main round">
+            <p>{children || 'Không tìm thấy trang'}</p>
+            <Link to={PATH.home} className="btn main round">
               Trang chủ
-            </a>
+            </Link>
           </section>
         </div>
       </div>
